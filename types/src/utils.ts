0661ad7c5df3c3906293a2791c57f71a3072a475
Felix Hanau <felix@cloudflare.com>
2026-01-13 03:08:06 +0000

    speculatively simplify configuration

--- types/src/utils.ts
+++ types/src/utils.ts
@@ -1,7 +1,8 @@
 import { existsSync } from "node:fs";
 import path from "node:path";
 
-// When building types from the upstream repo all paths need to be prepended by external/workerd/
+// When building types from the upstream repo all paths need to be prepended by
+// external/+dep_workerd+workerd/
 export function getFilePath(f: string): string {
   if (existsSync("external/+dep_workerd+workerd")) {
     return path.join("external", "+dep_workerd+workerd", f);
